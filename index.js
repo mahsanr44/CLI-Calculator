e767a6b3a4839f9374f0e45da8296f4b227fae71
Ahsan <mahsanr44@gmail.com>
2022-11-20 19:27:47 +0500

    first commit mycli

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prompt = require('prompt-sync')();
+function calculator() {
+    let number1 = Number(prompt("Enter First Number: "));
+    while (isNaN(number1)) {
+        number1 = Number(prompt("Please Enter a Number: "));
+    }
+    let number2 = Number(prompt("Enter Second Number: "));
+    while (isNaN(number2)) {
+        number2 = Number(prompt("Please Enter a Number:  "));
+    }
+    let operator = prompt("Enter an Operator from one of these,+, -, *, /, %: ");
+    if (operator == '+') {
+        console.log(`${number1} + ${number2} = ${number1 + number2}`);
+    }
+    else if (operator == '-') {
+        console.log(`${number1} - ${number2} = ${number1 - number2}`);
+    }
+    else if (operator == '*') {
+        console.log(`${number1} * ${number2} = ${number1 * number2}`);
+    }
+    else if (operator == '/') {
+        console.log(`${number1} / ${number2} = ${number1 / number2}`);
+    }
+    else if (operator == '%') {
+        console.log(`${number1} % ${number2} = ${number1 % number2}`);
+    }
+    else {
+        console.log("Oops! Invalid Operator.\nTry again.");
+    }
+}
+calculator();
+while (true) {
+    let check = prompt(`\nDo you want to calculate again? Type 'y' for yes> `);
+    if (check === 'y') {
+        calculator();
+    }
+    else {
+        console.log("Thank you!");
+        break;
+    }
+}
